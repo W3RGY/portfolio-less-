917736766d043bd826d1e2159276db0e3f52e1b2
Dima Osenkov <w3rgy1992@gmail.com>
2022-09-05 19:48:29 +0200

    11day

--- js/app.js
+++ js/app.js
@@ -106,7 +106,15 @@ $(function() {
 		nav.toggleClass("show");
 	});
 
+	/* ============== SMOOTH SCROLL ============== */
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
 
+		var blockId = $(this).data('scroll');
+		blockOffset = $(blockId).offset().top;
 
-
+		$("html, body").animate({
+			scrollTop: blockOffset
+		}, 500);
+	});
 });
